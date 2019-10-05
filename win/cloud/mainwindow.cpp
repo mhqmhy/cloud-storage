6f59e27cc79415e3080c0beed6d288ad3ddeb9c0
SheepHuan <justno@foxmail.com>
2019-10-05 20:12:49 +0800

    将TreeView放入了MainWindow

--- win/cloud/mainwindow.cpp
+++ win/cloud/mainwindow.cpp
@@ -15,7 +15,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     TreeView *tree=new TreeView();
     this->setCentralWidget(tree);
-//    QMenuBar * menuBar=new QMenuBar(this);
+//设置菜单栏
     QMenu *menu = ui->menubar->addMenu("文件");
     QAction *a1=new QAction(tr("上传"));
     menu->addAction(a1);
@@ -27,11 +27,16 @@ MainWindow::MainWindow(QWidget *parent)
     menu->addAction(a4);
     QAction *a5=new QAction(tr("删除"));
     menu->addAction(a5);
-    QMenu *menuIns = ui->menubar->addMenu("帮助");
-    QAction *a6=new QAction(tr("了解我们"));
-    menuIns->addAction(a6);
+    QMenu *menuSetting = ui->menubar->addMenu("帮助");
+    QAction *a6=new QAction(tr("更改根目录"));
+    menuSetting->addAction(a6);
     QAction *a7=new QAction(tr("产品介绍"));
-    menuIns->addAction(a7);
+    menuSetting->addAction(a7);
+    QMenu *menuIns = ui->menubar->addMenu("帮助");
+    QAction *a8=new QAction(tr("了解我们"));
+    menuIns->addAction(a8);
+    QAction *a9=new QAction(tr("产品介绍"));
+    menuIns->addAction(a9);
 }
 
 MainWindow::~MainWindow()
