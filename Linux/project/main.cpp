d40e4b940602f925f2953ba37b855558ed0a5dbe
SheepHuan <justno@foxmail.com>
2019-10-07 10:44:43 +0800

    项目移植到了Linux

---
+++ Linux/project/main.cpp
@@ -0,0 +1,41 @@
+#include <QApplication>
+#include <QAbstractItemModel>
+#include <QAbstractItemView>
+#include <QItemSelectionModel>
+#include <QCoreApplication>
+#include <QFileInfo>
+#include <QTextCodec>
+#include <QStringList>
+#include <QDateTime>
+#include <QDebug>
+#include <QDirModel>
+#include <QTreeView>
+#include <QListView>
+#include <QTableView>
+#include <QSplitter>
+#include <QFileSystemModel>
+#include <QModelIndex>
+#include "mainwindow.h"
+#include "TreeView.h"
+
+void fileSystem()
+{
+    QFileSystemModel model;
+    model.setRootPath(QDir::currentPath());
+    QTreeView tree;
+    tree.setModel(&model);
+    tree.setRootIndex(model.index(QDir::currentPath()));
+    tree.show();
+}
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow * w=new MainWindow();
+    w->show();
+
+
+
+  return a.exec();
+}
